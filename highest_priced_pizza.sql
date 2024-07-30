e95874b8a3af2135dddf773dfac896d2762914d4
Ashutosh Lakhera <44149436+ashulakhera@users.noreply.github.com>
2024-07-30 11:05:08 +0700

    Update highest_priced_pizza.sql

--- highest_priced_pizza.sql
+++ highest_priced_pizza.sql
@@ -8,3 +8,8 @@ FROM
     pizzas ON pizzas.pizza_type_id = pizza_types.pizza_type_id
 ORDER BY pizzas.price DESC
 LIMIT 1;
+
+#################EXAMPLE_OUTPUT################
+    
+price	name
+35.95	The Greek Pizza
