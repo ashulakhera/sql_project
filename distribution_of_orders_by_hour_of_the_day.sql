38b681364b35bef96baba7f63447c563e46a8029
Ashutosh Lakhera <44149436+ashulakhera@users.noreply.github.com>
2024-07-30 10:58:46 +0700

    Update distribution_of_orders_by_hour_of_the_day.sql

--- distribution_of_orders_by_hour_of_the_day.sql
+++ distribution_of_orders_by_hour_of_the_day.sql
@@ -7,4 +7,20 @@ FROM
 GROUP BY HOUR(order_time)
 ORDER BY order_count DESC;
 
-
+##########################EXAMPLE_OUTPUT###################
+hour	order_count
+12	2520
+13	2455
+18	2399
+17	2336
+19	2009
+16	1920
+20	1642
+14	1472
+15	1468
+11	1231
+21	1198
+22	663
+23	28
+10	8
+9	1
